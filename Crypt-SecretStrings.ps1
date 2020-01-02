0a9506fafcf5ce644cc4973e9fa70b750d3ff03c
neko3cs <minaton.0722.net@gmail.com>
2020-01-02 23:33:45 +0900

    コード整理

--- Crypt-SecretStrings.ps1
+++ Crypt-SecretStrings.ps1
@@ -1,110 +1,127 @@
 # PowerShell Core
 
+<#
+.SYNOPSIS
+    Encrypt or Decrypt with Triple DES.
+    Default is Encryption.
+.DESCRIPTION
+    1. .\Crypt-SecretStrings.ps1 -Value <Dcrypted String>
+    2. .\Crypt-SecretStrings.ps1 -Decrypt -Value <Encrypted String>
+.PARAMETER Decrypt
+    Decrypt '-Value' option string.
+.PARAMETER Value
+    The string you want to crypt.
+#>
+
 Param(
     [string]$Value,
-    [switch]$Decrypt,
-    [switch]$Help
+    [switch]$Decrypt
 )
 
-Import-Module ./Modules/Use-Disposable.psm1
+begin {
+    Import-Module ./Modules/Use-Disposable.psm1
 
-function Write-Usage {
-    Write-Output "usage: "
-    Write-Output "      1. .\Crypt-SecretStrings.ps1 -Value <Dcrypted String>"
-    Write-Output "      2. .\Crypt-SecretStrings.ps1 -Decrypt -Value <Encrypted String>"
-    Write-Output ""
-    Write-Output "  -Decrypt: Decrypt '-Value' option string."
-    Write-Output "  -Value  : The string you want to crypt."
-}
-
-if ($Help -or [string]::IsNullOrEmpty($Value)) {
-    Write-Usage
-    exit
-}
-
-# HACK: キーを環境変数に持ってモジュール化するか検討
-$SecretFileName = "Crypt-SecretStrings.secret"
-if (-not [System.IO.File]::Exists($SecretFileName)) {
-    $TripleDESCryptoServiceProvider = New-Object System.Security.Cryptography.TripleDESCryptoServiceProvider
-    $TripleDESCryptoServiceProvider.GenerateKey();
-    $TripleDESCryptoServiceProvider.GenerateIV();
-    @{
-        DesKey = [Convert]::ToBase64String($TripleDESCryptoServiceProvider.Key)
-        DesIv  = [Convert]::ToBase64String($TripleDESCryptoServiceProvider.IV)
-    } | ConvertTo-Json | Out-File -FilePath $SecretFileName
-}
-$Secret = Get-Content -Path $SecretFileName | ConvertFrom-Json
-
-function Get-EncriptString {
-    Param(
-        [string]$Value
-    )
-    
-    Use-Disposable (New-Object System.IO.MemoryStream) {
-        Param (
-            $MemoryStream
-        )
-
+    function New-SecretKeyAndIV {
         $TripleDESCryptoServiceProvider = New-Object System.Security.Cryptography.TripleDESCryptoServiceProvider
-        $Encryptor = $TripleDESCryptoServiceProvider.CreateEncryptor(
-            [Convert]::FromBase64String($Secret.DesKey),
-            [Convert]::FromBase64String($Secret.DesIv)
-        )
+        $TripleDESCryptoServiceProvider.GenerateKey();
+        $TripleDESCryptoServiceProvider.GenerateIV();
 
-        Use-Disposable (New-Object System.Security.Cryptography.CryptoStream(
-                $MemoryStream, $Encryptor, [System.Security.Cryptography.CryptoStreamMode]::Write)
-        ) {
-            Param(
-                $CryptoStream
-            )
-            $bytes = [System.Text.Encoding]::UTF8.GetBytes($Value)
-
-            $CryptoStream.Write($bytes, 0, $bytes.Length)
-            $CryptoStream.FlushFinalBlock()
-
-            return [Convert]::ToBase64String($MemoryStream.ToArray())
+        return @{
+            DesKey = [Convert]::ToBase64String($TripleDESCryptoServiceProvider.Key)
+            DesIv  = [Convert]::ToBase64String($TripleDESCryptoServiceProvider.IV)
         }
     }
-}
 
-function Get-DecriptString {
-    Param(
-        [string]$Value
-    )
-
-    $bytes = [Convert]::FromBase64String($Value)
-    Use-Disposable (New-Object System.IO.MemoryStream(, $bytes)) {
-        Param (
-            $MemoryStream
+    function Get-EncriptString {
+        Param(
+            [string]$Value,
+            [object]$Secret
         )
-
-        $TripleDESCryptoServiceProvider = New-Object System.Security.Cryptography.TripleDESCryptoServiceProvider
-        $Encryptor = $TripleDESCryptoServiceProvider.CreateDecryptor(
-            [Convert]::FromBase64String($Secret.DesKey),
-            [Convert]::FromBase64String($Secret.DesIv)
-        )
-
-        Use-Disposable (New-Object System.Security.Cryptography.CryptoStream(
-                $MemoryStream, $Encryptor, [System.Security.Cryptography.CryptoStreamMode]::Read)
-        ) {
-            Param(
-                $CryptoStream
+    
+        Use-Disposable (New-Object System.IO.MemoryStream) {
+            Param (
+                $MemoryStream
             )
 
-            Use-Disposable (New-Object System.IO.StreamReader($CryptoStream)) {
-                Param (
-                    $StreamReader
+            $TripleDESCryptoServiceProvider = New-Object System.Security.Cryptography.TripleDESCryptoServiceProvider
+            $Encryptor = $TripleDESCryptoServiceProvider.CreateEncryptor(
+                [Convert]::FromBase64String($Secret.DesKey),
+                [Convert]::FromBase64String($Secret.DesIv)
+            )
+
+            Use-Disposable (New-Object System.Security.Cryptography.CryptoStream(
+                    $MemoryStream, $Encryptor, [System.Security.Cryptography.CryptoStreamMode]::Write)
+            ) {
+                Param(
+                    $CryptoStream
+                )
+                $bytes = [System.Text.Encoding]::UTF8.GetBytes($Value)
+
+                $CryptoStream.Write($bytes, 0, $bytes.Length)
+                $CryptoStream.FlushFinalBlock()
+
+                return [Convert]::ToBase64String($MemoryStream.ToArray())
+            }
+        }
+    }
+
+    function Get-DecriptString {
+        Param(
+            [string]$Value,
+            [object]$Secret
+        )
+
+        $bytes = [Convert]::FromBase64String($Value)
+        Use-Disposable (New-Object System.IO.MemoryStream(, $bytes)) {
+            Param (
+                $MemoryStream
+            )
+
+            $TripleDESCryptoServiceProvider = New-Object System.Security.Cryptography.TripleDESCryptoServiceProvider
+            $Encryptor = $TripleDESCryptoServiceProvider.CreateDecryptor(
+                [Convert]::FromBase64String($Secret.DesKey),
+                [Convert]::FromBase64String($Secret.DesIv)
+            )
+
+            Use-Disposable (New-Object System.Security.Cryptography.CryptoStream(
+                    $MemoryStream, $Encryptor, [System.Security.Cryptography.CryptoStreamMode]::Read)
+            ) {
+                Param(
+                    $CryptoStream
                 )
 
-                return $StreamReader.ReadLine()
+                Use-Disposable (New-Object System.IO.StreamReader($CryptoStream)) {
+                    Param (
+                        $StreamReader
+                    )
+
+                    return $StreamReader.ReadLine()
+                }
             }
         }
     }
 }
+process {
+    if ($Help -or [string]::IsNullOrEmpty($Value)) {
+        Write-Usage
+        exit
+    }
 
-if ($Decrypt) {
-    Get-DecriptString -Value $Value | Write-Output
-}
-else {
-    Get-EncriptString -Value $Value | Write-Output
+    $Secret = $null
+    $SecretFileName = "Crypt-SecretStrings.secret"
+    if ([System.IO.File]::Exists($SecretFileName)) {
+        $Secret = Get-Content -Path $SecretFileName | ConvertFrom-Json
+    }
+    else {
+        # HACK: キーを環境変数に持ってモジュール化するか検討
+        $Secret = New-SecretKeyAndIV
+        $Secret | ConvertTo-Json | Out-File -FilePath $SecretFileName
+    }
+    
+    if ($Decrypt) {
+        return Get-DecriptString -Value $Value -Secret $Secret
+    }
+    else {
+        return Get-EncriptString -Value $Value -Secret $Secret
+    }
 }
