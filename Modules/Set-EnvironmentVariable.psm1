9395373b4629405cb3808196d2d95623d20031a5
neko3cs <minaton.0722.net@gmail.com>
2020-03-06 08:15:58 +0900

    todo: make set-envval script.

---
+++ Modules/Set-EnvironmentVariable.psm1
@@ -0,0 +1,17 @@
+#!pwsh
+
+<#
+.SYNOPSIS
+Set environment variable.
+
+.DESCRIPTION
+Set environment variable.
+You choose environment variable type: e.g., system environment variable and user environment variable.
+
+.PARAMETER EnvironmentVariableType
+Type of environment variable.
+You choose from system environment variable and user environment variable.
+
+#>
+
+# TODO: 書く
