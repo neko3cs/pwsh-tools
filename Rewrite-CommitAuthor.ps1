97bd8d6031d246d8bdca186977a5ad2dad44412e
neko3cs <minaton.0722.net@gmail.com>
2020-03-06 07:58:03 +0900

    一部実装（未完成）

--- Rewrite-CommitAuthor.ps1
+++ Rewrite-CommitAuthor.ps1
@@ -1,15 +1,37 @@
 # PowerShell Core
 
-$User = ""
-$EMail = ""
+param (
+    # [Parameter(Mandatory)]
+    [string]$GitUser,
+    # [Parameter(Mandatory)]
+    [string]$GitEmail,
+    [switch]$Help
+)
 
+function Write-Usage {
+    echo "usage:"
+    #TODO: usage書く
+}
+
+$GitUser = ""                     # DEBUG
+$GitEmail = "" # DEBUG
+
+if ($Help) {
+    Write-Usage
+    exit
+}
+if ([string]::IsNullOrEmpty($GitUser) -and [string]::IsNullOrEmpty($GitEmail)) {
+    Write-Usage
+    exit
+}
+
+# TODO: ここもスクリプト内部で完結させる
 $commits = @(
-    ""
 )
 
 # FIXME: git filter-branchは古いから使うなみたいな警告が出てログが書き換わらない
 foreach ($commit in $commits) {
     git filter-branch -f --env-filter `
-        "GIT_AUTHOR_NAME='${User}'; GIT_AUTHOR_EMAIL='${EMail}'; GIT_COMMITTER_NAME='${User}'; GIT_COMMITTER_EMAIL='${EMail}';" `
+        "GIT_AUTHOR_NAME='${GitUser}'; GIT_AUTHOR_EMAIL='${GitEmail}'; GIT_COMMITTER_NAME='${GitUser}'; GIT_COMMITTER_EMAIL='${GitEmail}';" `
         $commit
 }
