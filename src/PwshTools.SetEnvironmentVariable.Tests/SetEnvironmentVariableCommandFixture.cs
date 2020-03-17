38f9411e920ce3d758a564912d055fd7251dd6fa
neko3cs <minaton.0722.net@gmail.com>
2020-03-17 13:59:59 +0900

    Initialize SetEnvironmentVariable Project

---
+++ src/PwshTools.SetEnvironmentVariable.Tests/SetEnvironmentVariableCommandFixture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PwshTools.SetEnvironmentVariable.Tests
+{
+    public class SetEnvironmentVariableCommandFixture
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/PwshTools.SetEnvironmentVariable/SetEnvironmentVariableCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PwshTools.SetEnvironmentVariable
+{
+    public class SetEnvironmentVariableCommand
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
