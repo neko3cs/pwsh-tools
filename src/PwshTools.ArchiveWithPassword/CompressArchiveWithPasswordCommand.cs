a52b6f725ff2ede6f6dbe158557f6e2957a14702
neko3cs <minaton.0722.net@gmail.com>
2020-03-19 20:54:20 +0900

    init CompressArchiveWithPasswordCommand

---
+++ src/PwshTools.ArchiveWithPassword/CompressArchiveWithPasswordCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace PwshTools.CompressArchive
+{
+    [Cmdlet(VerbsData.Compress, "ArchiveWithPassword")]
+    [OutputType(typeof(string))]
+    [Alias("zip")]
+    public class CompressArchiveWithPasswordCommand : Cmdlet
+    {
+        [Parameter(
+            Position = 0,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true
+        )]
+        public string[] Path { get; set; }
+
+        [Parameter(
+            Position = 1,
+            ValueFromPipeline = false,
+            ValueFromPipelineByPropertyName = true
+        )]
+        public string DestinationPath { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+        }
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+        }
+    }
+}
