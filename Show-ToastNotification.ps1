8012807b5b24a9d92448d800d8e6cce6ae170cdb
neko3cs <minaton.0722.net@gmail.com>
2021-04-10 23:44:32 +0900

    ちょっと直した.

--- Show-ToastNotification.ps1
+++ Show-ToastNotification.ps1
@@ -1,32 +1,44 @@
-#!pwsh
+# Windows PowerShell
 
-Function Show-Toast {
-    [CmdletBinding()]
-    PARAM (
-        [Parameter(Mandatory = $true)][String] $title,
-        [Parameter(Mandatory = $true)][String] $message,
-        [Parameter(Mandatory = $true)][String] $detail
-    )
+<#
+.SYNOPSIS
+    Show Windows 10 Toast.
+.DESCRIPTION
+    Show Windows 10 Toast.
+    Toast is simple that consist of title and message.
+.PARAMETER Title
+    Toast title.
+.PARAMETER Message
+    Toast message.
+#>
 
-    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
-    [Windows.UI.Notifications.ToastNotification, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
-    [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] | Out-Null
+PARAM (
+    [Parameter(Mandatory)]
+    [String] $Title,
+    [Parameter(Mandatory)]
+    [String] $Message
+)
 
-    $app_id = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
-    $content = @"
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+[Windows.UI.Notifications.ToastNotification, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+[Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] | Out-Null
+
+# Create Toast UI Xml
+$content = @"
 <?xml version="1.0" encoding="utf-8"?>
 <toast>
     <visual>
         <binding template="ToastGeneric">
-            <text>$($title)</text>
-            <text>$($message)</text>
-            <text>$($detail)</text>
+            <text>$($Title)</text>
+            <text>$($Message)</text>
         </binding>
     </visual>
 </toast>
 "@
-    $xml = New-Object Windows.Data.Xml.Dom.XmlDocument
-    $xml.LoadXml($content)
-    $toast = New-Object Windows.UI.Notifications.ToastNotification $xml
-    [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($app_id).Show($toast)
-}
+$xml = New-Object Windows.Data.Xml.Dom.XmlDocument
+$xml.LoadXml($content)
+
+# Run Show Toast
+$toast = New-Object Windows.UI.Notifications.ToastNotification $xml
+$app_id = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
+[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($app_id).Show($toast)
