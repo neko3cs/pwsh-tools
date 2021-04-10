e3709d48b247f73ec11fe60985ed930e6b5604f0
neko3cs <minaton.0722.net@gmail.com>
2021-04-10 23:35:08 +0900

    不要スクリプトの削除（スキルが上がってその場でかけるようになったなど）

--- ConvertFrom-Base64String.ps1
+++
@@ -1,8 +0,0 @@
-#!pwsh
-
-param(
-    [string]$Base64String
-)
-
-$bytes = [System.Convert]::FromBase64String($Base64String);
-return [System.Text.Encoding]::UTF8.GetString($bytes);
--- ConvertTo-Base64String.ps1
+++
@@ -1,8 +0,0 @@
-#!pwsh
-
-param(
-    [string]$RawString
-)
-
-$bytes = [System.Text.Encoding]::UTF8.GetBytes($RawString);
-return [System.Convert]::ToBase64String($bytes);
