6742892452b9d37dd5482d586f390c4dc3fd7179
neko3cs <minaton.0722.net@gmail.com>
2022-04-23 21:51:34 +0900

    BinaryModule今後は別プロジェクトする（消した奴は別プロジェクトで似たモノがある）

--- binary_module/src/PwshTools.CryptSecretString/TripleDESCryptoSecretInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PwshTools.CryptSecretString
-{
-    public class TripleDESCryptoSecretInfo
-    {
-        public string Key { get; set; }
-        public string IV { get; set; }
-    }
-}
